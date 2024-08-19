8563b7937ba186bffcda64dfc82f7afa331dd7df
jaeyoung99-lee <leejy0715@naver.com>
2024-08-19 22:30:35 +0900

    Link // Navigate

--- src/page/Aboutpage.js
+++ src/page/Aboutpage.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Aboutpage = () => {
+  const navigate = useNavigate();
+  const goToHomepage = () => {
+    navigate("/"); // 홈으로 가고 싶어서 매개 변수에 홈을 의미하는 슬래시(/)를 적음
+  };
+
   return (
     <div>
       <h1>Aboutpage</h1>
+      <button onClick={goToHomepage}>Go to Homepage</button>
     </div>
   );
 };
