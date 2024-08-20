a2a1f60c0dc4af24c3f7a195db695a4cc53ec67a
jaeyoung99-lee <leejy0715@naver.com>
2024-08-21 02:47:02 +0900

    UseParams : URL의 파라미터값 읽어오기

--- src/page/ProductDetailPage.js
+++ src/page/ProductDetailPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const ProductDetailPage = () => {
-  return <div>ProductDetailPage</div>;
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>ProductDetailPage{id}</h1>
+    </div>
+  );
 };
 
 export default ProductDetailPage;
