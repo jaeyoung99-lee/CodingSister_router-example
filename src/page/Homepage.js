e4783047e3816297a9e2763522ac2a68950a5128
jaeyoung99-lee <leejy0715@naver.com>
2024-08-21 03:02:24 +0900

    useSearchParams : URL의 쿼리값 읽어오기

--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Homepage = () => {
+  const navigate = useNavigate();
+  const goToProductpage = () => {
+    navigate("/products?q=pants");
+  };
   return (
     <div>
       <h1>Homepage</h1>
       <Link to="/about">Go to Aboutpage</Link>
+      <button onClick={goToProductpage}>Go to Productpage</button>
     </div>
   );
 };
--- src/page/Productpage.js
+++ src/page/Productpage.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Productpage = () => {
+  let [query, setQuery] = useSearchParams();
+  console.log("쿼리값 :", query.get("q")); // useSearchParams를 이용해서 쿼리 부분을 불러옴
   return (
     <div>
       <h1>Show All Products!</h1>
