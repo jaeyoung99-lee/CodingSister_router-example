b7b739107495022164e0cd6ce4b57ae4cab4142c
jaeyoung99-lee <leejy0715@naver.com>
2024-08-21 03:21:07 +0900

    Redirect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Homepage from "./page/Homepage";
 import Aboutpage from "./page/Aboutpage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Productpage from "./page/Productpage";
 import ProductDetailPage from "./page/ProductDetailPage";
+import LoginPage from "./page/LoginPage";
+import { useState } from "react";
+import UserPage from "./page/UserPage";
 
 function App() {
+  const [authenticate, setAuthenticate] = useState(false); // false가 로그인 안한 상태
+
+  // PrivateRoute는 대문자로 시작하기 때문에 함수가 아니라 컴포넌트임
+  const PrivateRoute = () => {
+    return authenticate == true ? <UserPage /> : <Navigate to="/login" />;
+  };
   return (
     <div>
       <Routes>
@@ -13,6 +22,8 @@ function App() {
         <Route path="/about" element={<Aboutpage />} />
         <Route path="/products" element={<Productpage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/user" element={<PrivateRoute />} />
       </Routes>
     </div>
   );
